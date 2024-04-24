cf557502aa05135e9302e5d4300fc151890eb7f8
Brady Leavitt <brady.d.leavitt@gmail.com>
2024-04-24 17:43:12 -0400

    Added docker and included sql odbc dependency

--- app.py
+++ app.py
@@ -15,5 +15,14 @@ class AzureVanna(AzureAISearch_VectorStore, OpenAI_Chat):
 
 vn = AzureVanna(config={"model": os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT")})
 
-app = VannaFlaskApp(vn, title="Vanna.AI - With Azure AI Search and Azure OpenAI Chat")
+server = os.getenv('AZURE_SQL_DB_SERVER')
+database = os.getenv('AZURE_SQL_DB_NAME')
+username = os.getenv('AZURE_SQL_DB_USER')
+password = os.getenv('AZURE_SQL_DB_PASSWORD')
+
+conn_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+vn.connect_to_mssql(odbc_conn_str=conn_string) # You can use the ODBC connection string here
+
+app = VannaFlaskApp(vn,  title="Vanna.AI - With Azure AI Search and Azure OpenAI Chat")
 app.run()
